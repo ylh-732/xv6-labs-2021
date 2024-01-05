2b922c686c56f928f01d10ac8be886c0e340272e
ylh-732 <ylh.732@gmail.com>
2024-01-05 17:20:52 +0800

    finish ph

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,9 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET];
+
+
 
 double
 now()
@@ -47,6 +50,8 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  
+  pthread_mutex_lock(&locks[i]); 
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,7 +59,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -62,11 +67,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&locks[i]); 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(&locks[i]);
 
   return e;
 }
@@ -104,6 +110,11 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
+  
 
 
   if (argc < 2) {
