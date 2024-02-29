33fc23b309c073454809475f91e445ec0d27fb10
ylh-732 <ylh.732@gmail.com>
2024-02-29 15:05:19 +0900

    Update xargs

--- user/xargs.c
+++ user/xargs.c
@@ -2,57 +2,34 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
-void append_line_args(int index, char* xargs_argv[]) {
-    char buf[512];
-    char c;
-
-    int start = 0;
-    int end = 0;
-    
-    xargs_argv[index] = 0; 
-
-    while(read(0, &c, 1) == 1) {  
-        if (c == ' ' || c == '\n') {
-            buf[end++] = 0;
-            xargs_argv[index++] = buf + start;
-            start = end;
-
-            if (c == '\n') {
-                xargs_argv[index] = 0;
-                printf("");
-                break;
-            }
-        } else {
-            buf[end++] = c;
-        }    
-    }
-}
-
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(2, "Usage: xargs...\n");
         exit(1);
     }
 
-    char* xargs_argv[MAXARG];
+    char* xargs_argv[argc];
+    char buf[512];
 
     for (int i = 1; i < argc; i++) {
         xargs_argv[i - 1] = argv[i];
     }
 
     while (1) {
-        append_line_args(argc - 1, xargs_argv);
+        gets(buf, 512);
 
-        if (xargs_argv[argc - 1] == 0) {
+        if (buf[0] == '\0') {
             break;
         }
+        
+        buf[strlen(buf) - 1] = '\0';
+        xargs_argv[argc - 1] = buf;
 
         if (fork() == 0) {
             exec(argv[1], xargs_argv);
         } else {
             wait(0);
-        }    
+        }
     }
 
     exit(0);
